fc48d2f1b55fe9a89d39e03b0b2a99acd8c87604
Griffin Evans <griffinevans@protonmail.com>
2025-11-14 01:34:19 -0800

    refactor: Switch from rand to fastrand and import advice as mod

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 // This is free and unencumbered software released into the public domain.
 // Author: Griffin Evans <griffinevans@protonmail.com>
-use rand::Rng;
+mod advice;
+
+use advice::ADVICE;
+
 
 fn main() {
+    let rng: usize = fastrand::usize(..ADVICE.len());
 
-    let bytes = include_bytes!("advice");
-    let contents = String::from_utf8_lossy(bytes);
-    
-    let advice: Vec<&str> = contents.trim().split("\n%\n").collect();
-
-    let rng: usize = rand::rng().random_range(0..advice.len());
-
-    println!("{}", advice[rng]);
+    println!("{}", ADVICE[rng]);
 }
